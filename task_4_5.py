3be520f14083000626c7002cc644602c7e9513c8
ivan <ivan.pavlov.alk@gmail.com>
2021-03-02 20:58:01 +0200

    task 3 - Complited

--- task_4_5.py
+++ task_4_5.py
@@ -1,34 +1,34 @@
+from sys import argv
 import requests
 import requests.utils as utils
 from decimal import Decimal
 import datetime
 
 
-def curr_rate(curr):
+def curr_rate():
     """
     Get currency rate from bank
-    :param curr: name of currency
+    :param: name of currency
     :return: currency rate
     """
     response = requests.get('http://www.cbr.ru/scripts/XML_daily.asp')
     encodings = requests.utils.get_encoding_from_headers(response.headers)
     content = response.content.decode(encoding=encodings)
-    _bufdate = content.rfind('Date="')
-    _bufdate = content[_bufdate + 6:_bufdate + 16]
-    data = datetime.datetime.strptime(_bufdate, "%d.%m.%Y").date()
+    curr = argv
     try:
-        buf_index = content.find(curr)
+        buf_index = content.find(str(curr).upper())
         val = content.index('</Value>', buf_index)
         _bufvalue = content[val - 7:val]
         value = Decimal(_bufvalue.replace(',', '.')).quantize(Decimal('0.00'), rounding='ROUND_DOWN')
-        return value, data
+        _bufdate = content.rfind('Date="')
+        _bufdate = content[_bufdate + 6:_bufdate + 16]
+        data = datetime.datetime.strptime(_bufdate, "%d.%m.%Y").date()
+    except TypeError:
+        exit()
     except ValueError:
         print("None")
+    print(f'{value},{data}')
 
 
-cur = input("Enter currency: ").upper()
-try:
-    bufcur, bufdate = curr_rate(cur)
-    print(bufcur, bufdate)
-except TypeError:
-    exit()
+if __name__ == "__main__":
+    print(curr_rate())
