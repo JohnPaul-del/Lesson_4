5e43788ed3cf3c6146c81e7ca842fba66ca38636
ivan <ivan.pavlov.alk@gmail.com>
2021-03-02 21:34:30 +0200

    task 5 - Complited

--- task_1_2_3.py
+++ task_1_2_3.py
@@ -5,11 +5,6 @@ import datetime
 
 
 def curr_rate(curr):
-    """
-    Get currency rate from bank
-    :param curr: name of currency
-    :return: currency rate
-    """
     response = requests.get('http://www.cbr.ru/scripts/XML_daily.asp')
     encodings = requests.utils.get_encoding_from_headers(response.headers)
     content = response.content.decode(encoding=encodings)
@@ -17,18 +12,15 @@ def curr_rate(curr):
     _bufdate = content[_bufdate + 6:_bufdate + 16]
     data = datetime.datetime.strptime(_bufdate, "%d.%m.%Y").date()
     try:
-        buf_index = content.find(curr)
+        buf_index = content.find(str(curr).upper())
         val = content.index('</Value>', buf_index)
         _bufvalue = content[val - 7:val]
         value = Decimal(_bufvalue.replace(',', '.')).quantize(Decimal('0.00'), rounding='ROUND_DOWN')
-        return value, data
+        print(f'{value}, {data}')
     except ValueError:
         print("None")
 
 
-cur = input("Enter currency: ").upper()
-try:
-    bufcur, bufdate = curr_rate(cur)
-    print(bufcur, bufdate)
-except TypeError:
-    exit()
+if __name__ == "__main__":
+    cur = input("Enter currency: ")
+    curr_rate(cur)
--- task_4_5.py
+++ task_4_5.py
@@ -1,34 +1,5 @@
 from sys import argv
-import requests
-import requests.utils as utils
-from decimal import Decimal
-import datetime
+from task_1_2_3 import curr_rate
 
-
-def curr_rate():
-    """
-    Get currency rate from bank
-    :param: name of currency
-    :return: currency rate
-    """
-    response = requests.get('http://www.cbr.ru/scripts/XML_daily.asp')
-    encodings = requests.utils.get_encoding_from_headers(response.headers)
-    content = response.content.decode(encoding=encodings)
-    curr = argv
-    try:
-        buf_index = content.find(str(curr).upper())
-        val = content.index('</Value>', buf_index)
-        _bufvalue = content[val - 7:val]
-        value = Decimal(_bufvalue.replace(',', '.')).quantize(Decimal('0.00'), rounding='ROUND_DOWN')
-        _bufdate = content.rfind('Date="')
-        _bufdate = content[_bufdate + 6:_bufdate + 16]
-        data = datetime.datetime.strptime(_bufdate, "%d.%m.%Y").date()
-    except TypeError:
-        exit()
-    except ValueError:
-        print("None")
-    print(f'{value},{data}')
-
-
-if __name__ == "__main__":
-    print(curr_rate())
+curren = argv[1]
+curr_rate(curren)
